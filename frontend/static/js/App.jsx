a591d89f4c5dcd6eb17deb3ca3986060df3b2e0b
Petteri Vänttinen <58398715+PetteriV99@users.noreply.github.com>
2021-01-08 04:00:51 +0200

    stuff

--- frontend/static/js/App.jsx
+++ frontend/static/js/App.jsx
@@ -2,15 +2,22 @@
 import React from "react";
 import Hello from "./Hello";
 import { PageHeader } from "react-bootstrap";
+import HeaderBackgroundImage from '../images/bk_img_1.jpg';
+require('../css/fullstack.css');
 
 export default class App extends React.Component {
   render () {
     return (
       <PageHeader>
         <div className='header-contents'>
-          <Hello name='Rimini' />
+          {this.addHeaderImg()}
+          <Hello />
         </div>
       </PageHeader>
     );
+  }
+  addHeaderImg() {
+    let headerBg = new Image();
+    headerBg.src = HeaderBackgroundImage;
   }
 }
--- frontend/static/webpack.config.js
+++ frontend/static/webpack.config.js
@@ -1,21 +1,56 @@
-const webpack = require('webpack');const config = {
-    entry:  __dirname + '/js/index.jsx',
-    output: {
-        path: __dirname + '/dist',
-        filename: 'bundle.js',
-    },
-    module: {
-        rules: [
-          {
-            test: /\.jsx?/,
-            exclude: /node_modules/,
-            use: 'babel-loader'
-          }
-        ]
+const webpack = require('webpack'); 
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const config = {
+  entry: __dirname + '/js/index.jsx',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
       },
-    resolve: {
-        extensions: ['.js', '.jsx', '.css']
-    },
+      {
+        // For pure CSS - /\.css$/i,
+        // For Sass/SCSS - /\.((c|sa|sc)ss)$/i,
+        // For Less - /\.((c|le)ss)$/i,
+        test: /\.((c|sa|sc)ss)$/i,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              // Run `postcss-loader` on each CSS `@import`, do not forget that `sass-loader` compile non CSS `@import`'s into a single file
+              // If you need run `sass-loader` and `postcss-loader` on each CSS `@import` please set it to `2`
+              importLoaders: 1,
+              // Automatically enable css modules for files satisfying `/\.module\.\w+$/i` RegExp.
+              modules: { auto: true },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: 'file-loader'
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
+        loader: "url-loader",
+        options: {
+          limit: 8192,
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.css']
+  },
+  plugins: [new ExtractTextPlugin('styles.css')
+  ]
 };
 
 module.exports = config;
